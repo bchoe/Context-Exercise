2c96b14170a7904fded8708685ac11ac34da1c36
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-22 20:22:44 -1000

    dirty push

--- context.js
+++ context.js
@@ -15,28 +15,35 @@ var megaman = new Robot("Mega Man");
 var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
-
 // #1
-console.log('invoke add: ',add(0,1) ); //invoke add
+console.log('invoke add: ',add(0,1) );
+
 add(0,1);
+
+//invoke the add function with 2 arguments, 0 and 1
 
 // #2
 console.log('invoke add.call: ',add.call(voltron,2,3));
 
 add.call(voltron,2,3);
 
+/*invoke the add function within the context of the object named voltron,
+passing in 2 arguments, 2 and 3 (using call())*/
 
 // #3
 console.log('invoke add.apply: ',add.apply(optimus,[20,30]) );
+
 add.apply(optimus,[20,30]);
 
-// #4
+/*invoke the add function within the context of the object named optimus,
+in 2 arguments, 20 and 30 (using apply())*/
 
+// #4
 var calculate= add.bind(bender);
 calculate('drinking','beer');
 
 console.log(calculate('drinking ','beer'));
 
-/*While in the end you produced the correct result --
-however documents say to bind add to calculate then invoke calculate with the arguments drinking and beer.
-You should not be hardcoding values or arguments when you bind to other objects.*/
+/*bind the function add to a new function named calculate
+with the context of the object bender (using bind())
+invoke the calculate passing in 2 arguments, "drinking " and "beer"*/
